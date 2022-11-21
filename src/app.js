717ff648fa8d5d2930023f5c649c0772de03cdf0
Clem34280 <clement.mure@epsi.fr>
2022-11-21 15:30:40 +0100

    TDD - add class Triangle

--- src/app.js
+++ src/app.js
@@ -16,6 +16,45 @@ class Cube {
     }
 }
 
+class Triangle {
+    constructor(coteA, coteB, coteC) {
+        this.coteA = coteA;
+        this.coteB = coteB;
+        this.coteC = coteC;
+    }
+
+    getType(){
+        if (this.coteA == this.coteB == this.coteC){
+            return 'Equilatéral';
+        }
+        else if (this.coteA == this.coteB || this.coteB == this.coteC || this.coteA == this.coteC){
+            return 'Isocèle';
+        }
+        else if ((this.coteA == this.coteB && this.coteC^2 == this.coteA^2 + this.coteB^2) || (this.coteB == this.coteC && this.coteA^2 == this.coteB^2 + this.coteC^2) || (this.coteA == this.coteC && this.coteB^2 == this.coteA^2 + this.coteC^2)){
+            return 'Isocèle-rectangle';
+        }
+        else if (this.coteA^2 == this.coteB^2 + this.coteC^2 || this.coteB^2 == this.coteA^2 + this.coteC^2 || this.coteC^2 == this.coteA^2 + this.coteB^2){
+            return 'Rectangle';
+        }
+        else {
+            return 'Quelconque'
+        }
+    }
+
+    getPerimeter(){
+        let P = (this.coteA + this.coteB + this.coteC);
+        return P;
+    }
+
+    getSurface(){
+        let P = ((this.coteA + this.coteB + this.coteC)/2);
+        let S = Math.sqrt(P*(P-this.coteA)*(P-this.coteB)*(P-this.coteC));
+        let res = S.toFixed(2);
+        return res;
+    }
+}
+
 module.exports = {
-    Cube:Cube
+    Cube:Cube,
+    Triangle:Triangle
 }
